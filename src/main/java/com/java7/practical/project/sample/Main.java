0e496c383f531b0d29343ef152a19d272f30c582
swampus <dmitrijs.gavrilovs.swampus@gmail.com>
2020-05-23 18:29:58 +0300

    h2 :: console

--- src/main/java/com/java7/practical/project/sample/Main.java
+++ src/main/java/com/java7/practical/project/sample/Main.java
@@ -9,12 +9,20 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import org.h2.tools.Server;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        Parent root2 = FXMLLoader.load(getClass().getResource("/user_registration.fxml"));
+
         primaryStage.setTitle("Hello World !!");
 
         StackPane stackPane = new StackPane();
@@ -28,19 +36,19 @@ public class Main extends Application {
         });
 
         Button secondButton = null;
-        for(Node node : root.getChildrenUnmodifiable()){
+        for (Node node : root.getChildrenUnmodifiable()) {
             System.out.println(node.getId());
 
-            if(node.getId().equals("myBox")){
+            if (node.getId().equals("myBox")) {
                 HBox hBox = (HBox) node;
-                for(Node node2 : hBox.getChildren()){
-                    if(node2.getId().equals("myButton")){
+                for (Node node2 : hBox.getChildren()) {
+                    if (node2.getId().equals("myButton")) {
                         Button button1 = (Button) node2;
 
 
                         button1.setOnAction(e -> {
                             //Open information dialog that says hello
-             primaryStage.setScene(scene);
+                            primaryStage.setScene(scene);
                         });
                     }
                 }
@@ -49,13 +57,12 @@ public class Main extends Application {
         }
 
 
-
         stackPane.getChildren().add(button);
 
 
         HBox hbox = root.getChildrenUnmodifiable().stream()
-                .filter(t-> t.getId().equals("myBox"))
-                .map(t->(HBox) t)
+                .filter(t -> t.getId().equals("myBox"))
+                .map(t -> (HBox) t)
                 .findAny().get();
 
 
@@ -63,12 +70,35 @@ public class Main extends Application {
         hbox.getChildren().add(myButton);
 
 
-
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+        try {
+            Class.forName("org.h2.Driver");
+            Server server = Server.createWebServer().start();
+            System.out.println("port: " + server.getPort());
+            System.out.println("status: " + server.getStatus());
+            Connection con = DriverManager.getConnection("jdbc:h2:mem:testdb", "sa", "password");
+            Statement stmt = con.createStatement();
+            //stmt.executeUpdate( "DROP TABLE table1" );
+            stmt.executeUpdate("CREATE TABLE table1 ( user varchar(50) )");
+            stmt.executeUpdate("INSERT INTO table1 ( user ) VALUES ( 'Claudio' )");
+            stmt.executeUpdate("INSERT INTO table1 ( user ) VALUES ( 'Bernasconi' )");
+            ResultSet rs = stmt.executeQuery("SELECT * FROM table1");
+            while (rs.next()) {
+                String name = null;
+
+                name = rs.getString("user");
+                System.out.println(name);
+            }
+            stmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+
         launch(args);
     }
 }
